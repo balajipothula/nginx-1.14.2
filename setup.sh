867f8df4fb053064054ca2694091921365f37c16
BALAJI POTHULA <balan.pothula@gmail.com>
2019-04-23 12:22:31 +0530

    Update and rename install-docker.sh to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 
 # Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
 # Date        : 31 August 2016,
-# Description : Installing Docker on Ubuntu.
+# Description : Installing nginx Docker image on Ubuntu.
 
-# Note: Please run this script with root privilages.
+# Note: Please run this script with sudo privilage.
 
 # uninstalling old versions of docker.
 apt -y remove docker docker-engine docker.io
@@ -27,12 +27,6 @@ apt -y install apt-transport-https \
 # adding docker official gpg key.
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-# verifying gpg key.
-#apt-key fingerprint 0EBFCD88
-
-# deleting old docker gpg key(s).
-#apt-key del 2C52609D
-
 # setting up the stable docker repository.
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
@@ -42,6 +36,15 @@ apt update
 # installing latest version of docker ce.
 apt -y install docker-ce docker-ce-cli containerd.io
 
-# running docker as non-sudoer.
-# note: reboot required.
-usermod -a -G docker $USER
+# extracting "webapp" tar ball and removing.
+tar xzf $HOME/nginx-1.14.2/webapp.tar.gz -C $HOME/nginx-1.14.2/ && rm $HOME/nginx-1.14.2/webapp.tar.gz
+
+# pulling nginx-1.14.2 image from docker hub.
+docker pull balajipothula/nginx-1.14.2
+
+# running docker container with name(--name) "nginx" as daemon(-d),
+# stdin(-i) with volume(-v) "webapp" on port(-p) "80".
+docker run --name nginx -d -i -p 80:80 --privileged -v $HOME/nginx-1.14.2/webapp:/webapp balajipothula/nginx-1.14.2 sh
+
+# executing docker container by name with stdin(-i), starting tomcat server.
+docker exec -i nginx nginx -c /webapp/nginx/conf/nginx.conf
