9f37d7136f6154319f7c261614a66825e90ba0b9
BALAJI POTHULA <balan.pothula@gmail.com>
2019-04-27 13:12:45 +0530

    Update Docker-Help.sh

--- Docker-Help.sh
+++ Docker-Help.sh
@@ -1,1 +1,66 @@
+# tomcat-8.5.40
+## alpine linux image with openjdk8-jre, tomcat-native, sqlite, redis.
+### Author: BALAJI POTHULA <*balaji.pothula@techie.com*>
 
+#### Cloning tomcat8 from GitHub.
+git clone https://github.com/balajipothula/nginx-1.14.2.git
+
+##### Extracting "webapp" tar ball.
+tar xzf $HOME/nginx-1.14.2/webapp.tar.gz -C $HOME/nginx-1.14.2/ 
+
+##### Installing "docker" on Ubuntu OS and
+##### setting "tomcat" server with "web application".
+sudo sh $HOME/nginx-1.14.2/setup.sh
+
+##### Running docker as non-sudoer.
+##### Note: Reboot required.
+##### After Reboot sudo not required by "docker".
+sudo usermod -a -G docker $USER
+
+##### Pulling image from Docker Hub.
+sudo docker pull balajipothula/nginx-1.14.2
+
+##### Creating or Building image from Dockerfile.
+##### username/repo:tag.
+sudo docker build -t balajipothula/nginx-1.14.2 .
+
+##### Saving docker image offline into local machine.
+sudo docker save -o nginx-1.14.2.docker balajipothula/nginx-1.14.2
+
+##### Loading offline docker image into machine.
+sudo docker load -i nginx-1.14.2.docker
+
+##### Displaying docker images.
+sudo docker images
+
+##### Displaying docker running containers.
+sudo docker ps
+
+##### Displaying docker all containers.
+sudo docker ps -a
+
+##### Running docker image with volume(-v) stdin(-i) daemon(-d) with port(-p) 80 for NGINX.
+##### (It will create a volume inside the container)
+sudo docker run --name nginx -d -i -p 80:80 --privileged -v $HOME/nginx-1.14.2/webapp:/webapp balajipothula/nginx-1.14.2 sh
+
+##### Executing docker container by name with stdin(-i), startup  NGINX server.
+sudo docker exec -i webapp nginx -c /webapp/nginx/conf/nginx.conf
+
+# Executing docker container by name with stdin(-i), nginx process with config file
+# and Reloading nginx master process.
+sudo docker exec -i webapp nginx -c /webapp/nginx/conf/nginx.conf -s reload
+
+##### Login into docker container.
+sudo docker exec -i -t nginx sh
+
+##### Starting docker container with name.
+sudo docker start nginx
+
+##### Stoping  docker container with name.
+sudo docker stop nginx
+
+##### Removing docker container with name.
+sudo docker rm nginx
+
+##### Removing docker image with username/repo:tag.
+sudo docker rmi balajipothula/nginx-1.14.2
